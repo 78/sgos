958a1594511b3ab62793441f2c6acdeab9639079
gdxxhg <gdxxhg@75993106-7cd0-11de-8963-3b09bc248160>
2009-08-16 13:20:31 +0000

    add api.bxm

--- trunk/sgos2/services/api/api.c
+++ trunk/sgos2/services/api/api.c
@@ -1,40 +1,40 @@
 //API for SGOS2
 
 #define SYSCALL0(id, type,name) \
-type __stdcall sys_##name(void) \
+type __attribute__((stdcall)) sys_##name(void) \
 { \
 	long result; \
-	__asm__ volatile ("int $0xA1" \
+	__asm__ __volatile__ ("int $0xA1" \
 	: "=a" (result) \
 	: "a"(id), "c"(0)  ); \
 return (type) result; \
 }
 
 #define SYSCALL1(id, type,name,atype,a) \
-type __stdcall sys_##name(atype a) \
+type __attribute__((stdcall)) sys_##name(atype a) \
 { \
 	long result; \
-	__asm__ volatile ("int $0xA0" \
+	__asm__ __volatile__ ("int $0xA0" \
 	: "=a" (result) \
 	: "a"(id), "b"(a)  ); \
 	return (type) result; \
 }
 
 #define SYSCALL2(id, type,name,atype,a,btype,b) \
-type __stdcall sys_##name(atype a,btype b) \
+type __attribute__((stdcall)) sys_##name(atype a,btype b) \
 { \
 	long result; \
-	__asm__ volatile ("int $0xA0" \
+	__asm__ __volatile__ ("int $0xA0" \
 	: "=a" (result) \
 	: "a"(id), "b"(a), "c"(b) ); \
 	return (type) result; \
 }
 
 #define SYSCALL3(id, type,name,atype,a,btype,b,ctype,c) \
-type __stdcall sys_##name(atype a,btype b,ctype c) \
+type __attribute__((stdcall)) sys_##name(atype a,btype b,ctype c) \
 { \
 	long result; \
-	__asm__ volatile ("int $0xA0" \
+	__asm__ __volatile__ ("int $0xA0" \
 	: "=a" (result) \
 	: "a"(id), "b"(a), "c"(b), "d"(c) ); \
 	return (type) result; \
