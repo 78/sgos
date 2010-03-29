0e88e376a9806a0d10e17c72d2b336d1fb6e5c27
gdxxhg <gdxxhg@75993106-7cd0-11de-8963-3b09bc248160>
2010-03-29 08:49:38 +0000

    definition of size_t, clock_t ...

--- trunk/sgos2/include/types.h
+++ trunk/sgos2/include/types.h
@@ -7,22 +7,22 @@ typedef unsigned short		ushort;
 typedef unsigned int		uint;
 typedef unsigned char		byte;
 
-typedef unsigned long		size_t;
-typedef unsigned long		time_t;
-typedef unsigned long		clock_t;
+typedef unsigned int		size_t;
+typedef unsigned int		time_t;
+typedef unsigned int		clock_t;
 
 typedef unsigned char		t_8;
 typedef unsigned short		t_16;
-typedef unsigned			t_32;
+typedef unsigned		t_32;
 typedef unsigned long long	t_64
 ;
-typedef signed char			int8_t;
+typedef signed char		int8_t;
 typedef unsigned char		uint8_t;
-typedef short				int16_t;
+typedef short			int16_t;
 typedef unsigned short		uint16_t;
-typedef int					int32_t;
-typedef unsigned			uint32_t;
-typedef long long			int64_t;
+typedef int			int32_t;
+typedef unsigned		uint32_t;
+typedef long long		int64_t;
 typedef unsigned long long	uint64_t;
 
 #ifndef NULL
